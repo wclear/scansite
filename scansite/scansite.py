87960c79132d49907668f500cc506ef3a883f9e0
William Clear <william.j.clear@gmail.com>
2016-03-05 19:03:15 +0100

    Initial comments

---
+++ scansite/scansite.py
@@ -0,0 +1,9 @@
+# Hello fellow software developer! My name is Will and I 
+# would like to start by saying I don't know much about
+# Python or web crawlers. This is an adventure more to 
+# learn about these things rather than make top quality
+# software, although hopefully the end result is a good
+# one nevertheless.
+
+import sys;
+print(sys.version)
