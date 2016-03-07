4d4cb1680259cdf5b65d802ceedadb091b05c34c
William Clear <william.j.clear@gmail.com>
2016-03-07 17:53:16 +0100

    Woo hoo! We're checking status codes on website resources!

--- scansite/scansite.py
+++ scansite/scansite.py
@@ -17,11 +17,19 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 
+# This is an interesting one, it gives us an object that will
+# detect things like start and end tags it is reading HTML tags
+# we can listen in and act on these events: e.g. saving the
+# value of the href attribute.
+from html.parser import HTMLParser
+
 # This variable sitting up here by itself will later
 # become the base url from which all other web pages are
 # gathered. We will also limit our results to pages served
 # from the same base.
 baseURL = "";
+baseURLsecure = "";
+baseURLparts = None;
 
 # We also just want this to be a short implementation, not
 # accidentally overrun the web. So let's give it a max depth of
@@ -30,10 +38,92 @@ baseURL = "";
 # ignore it.
 maxDepth = 5;
 
+# To keep track of what pages that we have already scanned, let's
+# create a list for that. This list will be a list of lists, each
+# identified by the url of the given scanned page. The list within
+# the list will be the urls of pages that link to the key url. 
+# As well, the HTTP status code will be stored alongside each url.
+# So the list will look something like:
+# scannedpages = [
+#   ('http://example.com', 200, ['http://ilinktoexample.com']),
+#   ('http://me.com', 200, ['http://ilinktome.com', 'http://ilinktoexample.com'])
+# ]
+outcomes = []
+
+scannedpages = []
+
+# And we need a queue of pages that will be scanned
+unscannedpages = []
+
+class LinkParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global scannedpages
+        global unscannedpages
+        if tag == "a":
+            link = gethref(attrs)
+
+            # If we failed to get a link, we can leave
+            if link != False:
+                cleanlink = sanitizelink(link)
+
+                # If we failed to clean the link and make it useable, we can also leave
+                if cleanlink != False:
+
+                    # Finally, if we already know about this link, we don't need to scan it
+                    # again.
+                    if not((cleanlink in scannedpages) or (cleanlink in unscannedpages)):
+
+                        # The nesting at this point is ridiculous, we are on the
+                        # sixth indentation. Refactoring the code may make it a
+                        # bit more readable.
+                        unscannedpages.append(cleanlink)
+
 # We are going to need some helper functions for this scanner
 # to work. We put all these functions at the top of the file
 # so they will be defined when we run them at the bottom of the
 # file. This seems to be the way Python does things.
+
+# Starting with a simple one: if we are given a list of attributes
+# in the form [('name', 'value'),...] let's have a function called
+# gethref which will return the value of the first href attribute
+def gethref(attributes):
+    for att in attributes:
+        if att[0] == "href":
+            return att[1]
+    return False
+
+# We would like to be able to do a similar think with the src
+# attribute on script tags, let's do that in this getsrc function:
+def getsrc(attributes):
+    for att in attributes:
+        if att[0] == "src":
+            return att[1]
+    return False
+
+# We need full URLs that we can use to retrieve more web pages
+# with, partial URLs, paths like /about or my.jpg are not going
+# to get us anywhere. We also only want links that start with
+# the baseurl, since we just want to scan the one website. This
+# function could be extended to account for different parameters
+# but we will just separate web resources by their path for now
+def sanitizelink(url):
+    global baseURLparts
+    urlparts = urlparse(url)
+    cleanurl = "";
+    if url == "#":
+        return False
+    if not urlparts.netloc:
+        return baseURLparts.scheme + "://" + baseURLparts.netloc + urlparts.path
+    elif not urlparts.scheme:
+        return baseURLparts.scheme + "://" + urlparts.netloc + urlparts.path
+    elif not (urlparts.scheme == "http" or urlparts.scheme == "https"):
+        return False
+    else:
+        return baseURLparts.scheme + "://" + urlparts.netloc + urlparts.path
+    
+        
+    
+
 
 # This function will look at the parts of the given url string and
 # determine if we have everything we need to be able to use this
@@ -85,18 +175,30 @@ def validateUrl(url):
 # scan. That is, every other URL scanned should be in the same
 # directory or a subdirectory of the given URL.
 def scanBase(url):
+    global baseURL
+    global baseURLsecure
+    global unscannedpages
+    global baseURLparts
     try:
         webPage = urlopen(url)
 
         # We get the URL a second time here in case a redirect was used.
-        finalURL = webPage.geturl()
-        if (finalURL.lower() == url.lower()):
-            print("Scanning...")
-
         # If there was a redirect, let's check with our user if it 
-        # is OK to use it.
-        elif (input("A redirect was used to show the page: {0}. Type 'y' to continue: ".format(finalURL)) == 'y'):
-            print("Scanning...")
+        # is OK to use it. Otherwise, let's start scanning!
+        finalURL = webPage.geturl()
+        if finalURL.lower() == url.lower() or input("A redirect was used to show the page: {0}. Type 'y' to continue: ".format(finalURL)) == 'y':
+
+            # Woo hoo! We've got a URL that works, let's start scanning for errors
+            # on this website.
+            baseURLparts = urlparse(finalURL)
+            baseURL = "http://" + baseURLparts.netloc + baseURLparts.path
+            baseURLsecure = "https://" + baseURLparts.netloc + baseURLparts.path
+            unscannedpages.append(baseURL)
+            scan()
+
+        # The user was not happy with the URL, let's get out of here. It may
+        # be more user-friendly to give them another opportunity to write a
+        # new URL, but for now, let's take this easy way out.
         else:
             print("Goodbye")
 
@@ -117,8 +219,74 @@ def scanBase(url):
     # above will end up here.
     except WindowsError as err:
         print("Unable to connect. Please check your connection and the URL and try again. Full error: {0}".format(err))
+
+# This function will scan a given url, checking its HTTP code
+# then checking 
+def scan():
+    global unscannedpages
+    target = unscannedpages.pop()
+    moretogo = True;
+    try:
+        while moretogo:
+            success = False
+            try:
+                result = urlopen(target)
+                success = True
+            except:
+                print("Error opening: {0}".format(target))
+
+            rescode = 0 if not success else result.getcode()
+            if rescode == 200 and sharesbase(target):
+                try:
+                    scrapurls(target, result.read().decode('utf-8'))
+                    print("Scanned {1}".format(rescode, target))
+                except UnicodeDecodeError:
+                    print("Skipping non Unicode resource: {0}".format(target))
+            elif success:
+                recordresult(target, rescode)
+            try:
+                target = unscannedpages.pop()
+            except:
+                moretogo = False
+        showresult()
+    except KeyboardInterrupt:
+        print("Scan interrupted")
+
+
+# We need a function to show the outcomes of our hard work.
+def showresult():
+    global outcomes
+    print("Results")
+    print("HTTP Code | URL")
+    for result in outcomes:
+        print("{0} {1}".format(result[1], result[0]))
+
+def recordresult(url, code):
+    global outcomes
+    global scannedpages
+    outcomes.append((url, code, []))
+    scannedpages.append(url)
+
+# It would be nice if we could quickly check whether the URL
+# shares the same base as our original path. The sharesbase
+# function is the answer to this.
+def sharesbase(url):
+    global baseURL
+    global baseURLsecure
+    return url.startswith(baseURL) or url.startswith(baseURLsecure)
     
-    
+
+
+# This function is passed a HTML string. The HTML will be
+# parsed and as link tags are discovered, they will be added
+# to the unscanned pages list.
+def scrapurls(url, html):
+    global outcomes
+    global scannedpages
+    outcomes.append((url, 200, []))
+    scannedpages.append(url)
+    parser = LinkParser()
+    parser.feed(html)
 
 # Finally, after writing those functions we can look at
 # actually running the program.
